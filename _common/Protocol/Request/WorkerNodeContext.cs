86a3a87db2b8c872db6429668c5f58b9bd2ab7bc
Nikita Putintsev <Putintsevnikita@gmail.com>
2014-10-05 18:18:40 +0700

    Constructing request handler

---
+++ _common/Protocol/Request/WorkerNodeContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _common.NodeInterfaces;
+
+namespace _common.Protocol.Request
+{
+    class WorkerNodeContext
+    {
+        public readonly IAsyncLibraryManager LibraryManager;
+        public readonly IAsyncLoadManager LoadManager;
+        public readonly IAsyncSystemInfo SystemInfo;
+        public readonly IAsyncWorkerManager WorkerManager;
+        public WorkerNodeContext(IAsyncLibraryManager libraryManager, IAsyncLoadManager loadManager, 
+            IAsyncSystemInfo systemInfo, IAsyncWorkerManager workerManager)
+        {
+            LibraryManager = libraryManager;
+            LoadManager = loadManager;
+            SystemInfo = systemInfo;
+            WorkerManager = workerManager;
+        }
+    }
+}
