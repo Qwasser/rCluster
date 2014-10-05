02c9cf9b3e1efbb5d092749320ddb52ed2dd316a
Nikita Putintsev <Putintsevnikita@gmail.com>
2014-10-06 01:15:38 +0700

    some listeners

---
+++ _common/NodeInterfaces/ISystemInfoListener.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _common.NodeInterfaces
+{
+    public interface ISystemInfoListener
+    {
+        void OnMemoryRetreived(long memory);
+        void OnLoadRetreived(long load);
+    }
+}
---
+++ _common/NodeInterfaces/IWorkerManagerListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _common.NodeInterfaces
+{
+    interface IWorkerManagerListener
+    {
+        void OnCountRetreived(int count);
+        void OnError(string error);
+        void OnRedisIpRetrived(string ip);
+        void OnWorkersLoadRetreived(int load);
+        void OnWorkersMemoryRetreived(long memory);
+    }
+}
