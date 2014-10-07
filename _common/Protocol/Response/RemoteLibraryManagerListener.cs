c0f3d0ee7a7ad6a9f189b642af3f535ceeb2d6c2
Nikita Putintsev <Putintsevnikita@gmail.com>
2014-10-07 19:02:03 +0700

    Response interfaces

---
+++ _common/Protocol/Response/RemoteLibraryManagerListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _common.NodeInterfaces;
+
+namespace _common.Protocol.Response
+{
+    class RemoteLibraryManagerListener: ILibraryManagerListener
+    {
+        private readonly IResponseSender _sender;
+        public RemoteLibraryManagerListener(IResponseSender sender)
+        {
+            this._sender = sender;
+        }
+        public void OnLibraryListRetreived(List<string> libraryList)
+        {
+            _sender.SendResponse(new LibraryListRetrivedResponse(libraryList));
+        }
+
+        public void OnHasLibrary(string libraryName, bool hasLibrary)
+        {
+            _sender.SendResponse(new HasLibraryResponse(libraryName, hasLibrary));
+        }
+
+        public void OnLibraryInstalled(string libraryName)
+        {
+            _sender.SendResponse(new LibraryInstallResponse(libraryName));
+        }
+
+        public void OnFailure(string msg)
+        {
+            _sender.SendResponse(new LibraryFailureResponse(msg));
+        }
+    }
+}
