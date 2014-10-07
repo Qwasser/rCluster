0b00ff55382fe716ca7c3c77db10c8e1d6431469
alexanderzyryanov <alexander.zyryanov44@gmail.com>
2014-10-07 14:12:10 +0700

    Add System info

---
+++ WorkerNode/SystemInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkerNode
+{
+    class SystemInfo : ISystemInfo
+    {
+        public long GetMemory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLoad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
