191c1d0818eaa674362494d59f0f0f5a02361bad
Nikita Putintsev <Putintsevnikita@gmail.com>
2014-10-22 15:00:22 +0700

    Closing proxies on exit

--- MasterNodeApp/MainWindow.xaml.cs
+++ MasterNodeApp/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Documents;
 using NodeProxy;
 using _common.SocketConnection;
 
@@ -9,6 +12,7 @@ namespace MasterNodeApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly List<NodeProxy.NodeProxy> _proxies = new List<NodeProxy.NodeProxy>();
         public MainWindow()
         {
             InitializeComponent();
@@ -16,6 +20,7 @@ namespace MasterNodeApp
 
             var masterNodeSocket = new MasterNodeSocket();
             var nodeProxy = new NodeProxy.NodeProxy(masterNodeSocket, masterNodeSocket, new NodeProxyConfiguration("host 1", "127.0.0.1", 6700));
+            _proxies.Add(nodeProxy);
 
             var host1 = new NodeProxyGui(nodeProxy)
             {   
@@ -27,6 +32,14 @@ namespace MasterNodeApp
             HostStackPanel.UpdateLayout();
         }
 
-        
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            foreach (NodeProxy.NodeProxy proxy in _proxies)
+            {
+                proxy.Disconnect();
+            }
+        }
+
+
     }
 }
